bb5a81bcdf041690a062c523129c7c0884a2b032
root <root@vmi337013.contaboserver.net>
2020-01-19 13:42:43 +0100

    add some handlers

--- packages/posts-api/src/index.ts
+++ packages/posts-api/src/index.ts
@@ -1,8 +1,35 @@
-export const PostCreateType = 'PostCreate'
-
-export const PostCreate = Symbol('PostCreate')
+// Messages
 
 export interface PostCreate {
-    type: "PostCreate"
+    type: 'PostCreate'
+    req_id: string
     content: string
 }
+
+export const PostCreateType = 'PostCreate'
+
+export interface PostCreateSuccess {
+    type: 'PostCreateSuccess'
+    req_id: string
+    id: string
+}
+
+export const PostCreateSuccessType = 'PostCreateSuccess'
+
+export interface PostCreateFailed {
+    type: 'PostCreateFailed'
+    req_id: string
+    id: string
+}
+
+export const PostCreateFailedType = 'PostCreateFailed'
+
+
+export interface UnknownRequest {
+    type: 'UnknownRequest'
+    req_id: string
+    error: Object
+}
+
+export const UnknownRequestType = 'UnknownRequest'
+
--- packages/posts-srv/src/controllers/APIController.ts
+++ packages/posts-srv/src/controllers/APIController.ts
@@ -1,21 +1,37 @@
-import { Request, Response } from 'express';
-import { Controller, Middleware, Get, Put, Post, Delete } from '@overnightjs/core';
-import { PostCreate, PostCreateType } from '@msa/posts-api'
+import { Request, Response } from 'express'
+import { Controller, Middleware, Get, Put, Post, Delete } from '@overnightjs/core'
+import { PostCreate, PostCreateType, PostCreateSuccess, UnknownRequest } from '@msa/posts-api'
 
 @Controller('api')
 export class APIController {
 
     @Post()
     private async postMessage(req: Request, res: Response) {
-        var msg = 'None'
-        switch (req.body.type) {
-            case PostCreateType: {
-                msg = "YES"
+        if ((req.body.type) === PostCreateType) {
+            const msg_in: PostCreate = req.body
+            const msg: PostCreateSuccess = {
+                type: 'PostCreateSuccess',
+                req_id: msg_in.req_id,
+                id: 'sdfhjsdljf24243234'
             }
+            res.status(200).json({
+                message: msg,
+            })
+
+        } else {
+            const msg_in = req.body
+
+            const msg: UnknownRequest = {
+              type: 'UnknownRequest',
+              req_id: msg_in.req_id,
+              error: {
+                  'global': 'Request unknown'
+              }
+            }
+            res.status(405).json({
+                message: msg,
+            })
         }
 
-        res.status(200).json({
-            message: msg,
-        });
     }
 }
